c40e9a775b252f9919904b338a29ed71ae776596
Jimena92 <jimena.flores.92@gmail.com>
2023-03-11 23:43:43 -0600

    Primera versión

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 console.log(cipher);
+
+window.addEventListener("load", inicio, true);
+
+function inicio() {
+  document.getElementById("m1").addEventListener("keyup", function () {
+    this.value = this.value.toUpperCase();
+  });
+  document.getElementById("cifrar").addEventListener("click"),
+    function () {
+      let m1 = document.getElementById("m1").value;
+      let espacio = document.getElementById("espacio").value;
+      document.getElementById("m2").value = cifrar(m1, espacio);
+    };
+  document.getElementById("descifrar").addEventListener("click"),
+    function () {
+      let m1 = document.getElementById("m1").value;
+      let espacio = document.getElementById("espacio").value;
+      document.getElementById("m2").value = descifrar(m1, espacio);
+    };
+}
