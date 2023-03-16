6635238b1c8f552657d5c849d32fda7d4d6df63b
Jimena92 <jimena.flores.92@gmail.com>
2023-03-15 18:05:14 -0600

    Versión 4

--- src/index.js
+++ src/index.js
@@ -3,40 +3,55 @@ import cipher from "./cipher.js";
 console.log(cipher);
 
 window.addEventListener("load", inicio, true);
-//Esta función es para acceder a los ID que se utilizarán del DOM
+//Esta función es para acceder a los ID del DOM
 
 function inicio() {
   document.getElementById("m1").addEventListener("keyup", function () {
     this.value = this.value.toUpperCase();
   });
   document.getElementById("cifrar").addEventListener("click", function () {
-    const texto = document.getElementById("m1").value;
-    const desplazamiento = document.getElementById("espacio").value;
-    document.getElementById("m2").value = cifrar(texto, desplazamiento);
+    const string = document.getElementById("m1").value;
+    const offset = document.getElementById("espacio").value;
+    document.getElementById("m2").value = encode(offset, string);
   });
   document.getElementById("descifrar").addEventListener("click", function () {
-    const texto = document.getElementById("m1").value;
-    const desplazamiento = document.getElementById("espacio").value;
-    document.getElementById("m2").value = descifrar(texto, desplazamiento);
+    const string = document.getElementById("m1").value;
+    const offset = document.getElementById("espacio").value;
+    document.getElementById("m2").value = decode(offset, string);
   });
 }
 
-function cifrar(texto, desplazamiento) {
+function encode(offset, string) {
   let resultado = "";
   const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   //Este módulo es para aceptar un desplazamiento negativo
-  desplazamiento = ((desplazamiento % 26) + 26) % 26;
+  offset = ((offset % 26) + 26) % 26;
   /*Se crea una condicional y un bucle para saber el desplazamiento y a qué letra se va a llegar, lo logré pero
- siento que falta algo más, además no he metido lo de ASCII*/
-  if (texto) {
-    for (let i = 0; i < texto.length; i++) {
-      if (letras.indexOf(texto[i]) !== -1) {
-        const posicion = (letras.indexOf(texto[i]) + desplazamiento) % 26;
+ no he metido lo de ASCII*/
+  if (string) {
+    for (let i = 0; i < string.length; i++) {
+      if (letras.indexOf(string[i]) !== -1) {
+        const posicion = (letras.indexOf(string[i]) + offset) % 26;
         resultado += letras[posicion];
-      } else resultado += texto[i];
+      } else resultado += string[i];
     }
   }
   return resultado;
 }
 
-/*Aquí haré la función pero ahora para descifrar, aún no entiendo bien como. Aunque creo que es algo parecida a cifrar*/
+/*La función descifrar es muy parecida  a cifrar sólo se coloca en negativo*/
+
+function decode(offset, string) {
+  let resultado = "";
+  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  offset = ((offset % 26) + 26) % 26;
+  if (string) {
+    for (let i = 0; i < string.length; i++) {
+      if (letras.indexOf(string[i]) !== -1) {
+        const posicion = (letras.indexOf(string[i]) - offset) % 26;
+        resultado += letras[posicion];
+      } else resultado += string[i];
+    }
+  }
+  return resultado;
+}
