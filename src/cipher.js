b8bafdc0561d66c7d05877d0cd40e7135c79719f
Jimena92 <jimena.flores.92@gmail.com>
2023-03-18 17:48:07 -0600

    Versión final

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,53 @@
 const cipher = {
-  // ...
+  encode: function (offset, string) {
+    if (typeof string !== "string" || string.length === 0) {
+      throw new TypeError("No debe dejar vacío el campo.");
+    }
+    let resultado = "";
+    //Este módulo es para aceptar un desplazamiento negativo, por eso se repite
+    offset = ((offset % 26) + 26) % 26;
+    if (string) {
+      for (let i = 0; i < string.length; i++) {
+        //Por fin logré integrar el método ASCII
+        const currentCharCode = string.charCodeAt(i);
+        if (currentCharCode >= 65 && currentCharCode <= 90) {
+          let posicion = (currentCharCode - 65 + offset) % 26;
+          if (posicion < 0) {
+            posicion += 26;
+          }
+          resultado += String.fromCharCode(posicion + 65);
+        } else {
+          resultado += string[i];
+        }
+      }
+    }
+    return resultado;
+  },
+
+  //Para el decode es prácticamente lo mismo sólo en la condiconal de currentCharCode, la variable posicón queda en negativo
+
+  decode: function (offset, string) {
+    if (typeof string !== "string" || string.length === 0) {
+      throw new TypeError("No debe dejar vacío el campo.");
+    }
+    let resultado = "";
+    offset = ((offset % 26) + 26) % 26;
+    if (string) {
+      for (let i = 0; i < string.length; i++) {
+        const currentCharCode = string.charCodeAt(i);
+        if (currentCharCode >= 65 && currentCharCode <= 90) {
+          let posicion = (currentCharCode - 65 - offset) % 26;
+          if (posicion < 0) {
+            posicion += 26;
+          }
+          resultado += String.fromCharCode(posicion + 65);
+        } else {
+          resultado += string[i];
+        }
+      }
+    }
+    return resultado;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import cipher from "./cipher.js";
 
-console.log(cipher);
-
 window.addEventListener("load", inicio, true);
-//Esta función es para acceder a los ID del DOM
+
+//Esta función es para acceder a los ID que se utilizarán del DOM
 
 function inicio() {
   document.getElementById("m1").addEventListener("keyup", function () {
@@ -12,46 +11,11 @@ function inicio() {
   document.getElementById("cifrar").addEventListener("click", function () {
     const string = document.getElementById("m1").value;
     const offset = document.getElementById("espacio").value;
-    document.getElementById("m2").value = encode(offset, string);
+    document.getElementById("m2").value = cipher.encode(offset, string);
   });
   document.getElementById("descifrar").addEventListener("click", function () {
     const string = document.getElementById("m1").value;
     const offset = document.getElementById("espacio").value;
-    document.getElementById("m2").value = decode(offset, string);
+    document.getElementById("m2").value = cipher.decode(offset, string);
   });
-}
-
-function encode(offset, string) {
-  let resultado = "";
-  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  //Este módulo es para aceptar un desplazamiento negativo
-  offset = ((offset % 26) + 26) % 26;
-  /*Se crea una condicional y un bucle para saber el desplazamiento y a qué letra se va a llegar, lo logré pero
- no he metido lo de ASCII*/
-  if (string) {
-    for (let i = 0; i < string.length; i++) {
-      if (letras.indexOf(string[i]) !== -1) {
-        const posicion = (letras.indexOf(string[i]) + offset) % 26;
-        resultado += letras[posicion];
-      } else resultado += string[i];
-    }
-  }
-  return resultado;
-}
-
-/*La función descifrar es muy parecida  a cifrar sólo se coloca en negativo*/
-
-function decode(offset, string) {
-  let resultado = "";
-  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  offset = ((offset % 26) + 26) % 26;
-  if (string) {
-    for (let i = 0; i < string.length; i++) {
-      if (letras.indexOf(string[i]) !== -1) {
-        const posicion = (letras.indexOf(string[i]) - offset) % 26;
-        resultado += letras[posicion];
-      } else resultado += string[i];
-    }
-  }
-  return resultado;
 }
