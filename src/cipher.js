6e2cb2c7aeea64950b087ca55cec71c7b19560ba
Jimena92 <jimena.flores.92@gmail.com>
2023-03-22 19:54:15 -0600

    Se agregaron comentarios

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,40 @@
+// Objeto cipher con dos métodos para cifrar y descifrar un string.
 const cipher = {
   encode: function (offset, string) {
+    // Se comprueba que el parámetro 'string' no esté vacío.
     if (typeof string !== "string" || string.length === 0) {
       throw new TypeError("No debe dejar vacío el campo.");
     }
+
+    // Se inicializa la variable 'resultado' como string vacío.
     let resultado = "";
-    //Este módulo es para aceptar un desplazamiento negativo, por eso se repite
+
+    // Se establece el valor de 'offset' para que esté en el rango 0-25.
     offset = ((offset % 26) + 26) % 26;
+
+    // Se comprueba si el parámetro 'string' es un string válido, por eso se usa un If.
     if (string) {
+      // Se agrega un for y se itera por cada caracter en el string
       for (let i = 0; i < string.length; i++) {
-        //Por fin logré integrar el método ASCII
+        /*Por fin logré integrar el método ASCII, acá entendí que se obtiene el código ASCII del caracter actual*/
         const currentCharCode = string.charCodeAt(i);
+        /* Esta parte es para saber si el caracter es una letra mayúscula en ASCII y si cumple con eso, entonces se cifra.*/
         if (currentCharCode >= 65 && currentCharCode <= 90) {
+          // Se calcula la posición del caracter cifrado en el alfabeto.
           let posicion = (currentCharCode - 65 + offset) % 26;
+          // Si la posición es negativa, se ajusta para que quede en el rango de 0 a 25.
           if (posicion < 0) {
             posicion += 26;
           }
+          // Se agrega la letra cifrada al string "resultado".
           resultado += String.fromCharCode(posicion + 65);
+          // Si el caracter actual no es una letra mayúscula, se agrega tal cual al string "resultado".
         } else {
           resultado += string[i];
         }
       }
     }
+    // Se retorna el string 'resultado' cifrado
     return resultado;
   },
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
+// importar el módulo cipher.js en el archivo actual
 import cipher from "./cipher.js";
 
 window.addEventListener("load", inicio, true);
 
-//Esta función es para acceder a los ID que se utilizarán del DOM
+/*Esta función es para acceder a los ID que se utilizarán del DOM y se ejecutará en el evento load*/
 
 function inicio() {
+  // Se agrega un evento de teclado a la entrada con el id "m1"
+  // la función del evento convierte el valor de la entrada en mayúsculas
   document.getElementById("m1").addEventListener("keyup", function () {
     this.value = this.value.toUpperCase();
   });
+
+  /* Se agrega un evento de clic al botón con el id "cifrar"
+  La función del evento toma el valor de la entrada con el id "m1" y el valor del espacio de cifrado (offset), luego llama a la función "encode" del módulo cipher.js con los parámetros anteriores y actualiza el valor de la entrada con el id "m2" con el resultado cifrado.*/
+
   document.getElementById("cifrar").addEventListener("click", function () {
     const string = document.getElementById("m1").value;
     const offset = document.getElementById("espacio").value;
     document.getElementById("m2").value = cipher.encode(offset, string);
   });
+
+  //Se realiza lo mismo que lo anterior, pero esta vez con la función decode
   document.getElementById("descifrar").addEventListener("click", function () {
     const string = document.getElementById("m1").value;
     const offset = document.getElementById("espacio").value;
